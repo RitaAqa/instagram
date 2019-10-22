db4eb8bab73af70dd61a9d5a564fb899b79ae8dc
margaritaleonidova <margarita.leonidova@gmail.com>
2019-10-22 10:11:57 -0500

    first test

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from pages.login_page import LoginPage
+
+driver = webdriver.Chrome("C:/chromedriver.exe")
+driver.implicitly_wait(10)
+driver.get("https://www.instagram.com/accounts/login/")
+
+login_page = LoginPage(driver)
+login_page.enter_username("pyautomation")
+login_page.enter_password("Ab123456!")
+login_page.click_login()
